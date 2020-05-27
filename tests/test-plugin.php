9593b4d6d372fe0295a33975d57d1c9c7ec377a1
Nicolas Juen <njuen@beapi.fr>
2020-05-27 23:16:38 +0200

    debug on actions

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -82,6 +82,8 @@ class WP_HTTP_BLACKLIST extends WP_UnitTestCase {
 		// Old entry ok
 		$result = wp_remote_get('https://connect.advancedcustomfields.com');
 		$this->assertFalse( is_wp_error( $result ) );
+		var_dump($result);
+		die();
 
 		@unlink( __DIR__.'/blacklist_readable.txt' );
 
