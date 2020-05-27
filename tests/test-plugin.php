d425259c96bcd25f88f7a7d5ccc85b0d4c0945b8
Nicolas Juen <njuen@beapi.fr>
2020-05-27 21:46:49 +0200

    Split up tests, remove files after test ended

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -6,18 +6,46 @@
  */
 
 /**
- * Sample test case.
+ * Basic plugin tests.
  */
 class WP_HTTP_BLACKLIST extends WP_UnitTestCase {
+	private function filter_file_redable() {
+		add_filter('wp_http_blacklist_file', function() {
+			@unlink( __DIR__.'/blacklist_readable.txt' );
+			file_put_contents( __DIR__.'/blacklist_readable.txt', 
+				'google.fr  
+				beapi.fr
+				' 
+			);
+			return __DIR__.'/blacklist_readable.txt';
+		});
+	}
 
-	/**
-	 * A single example test.
-	 */
-	public function test_default() {
+	private function filter_file_unredable() {
+		add_filter('wp_http_blacklist_file', function() {
+			@unlink( __DIR__.'/blacklist_unreadable.txt' );
+			file_put_contents( __DIR__.'/blacklist_unreadable.txt', 
+				'google.fr  
+				beapi.fr
+				' 
+			);
+			chmod(  __DIR__.'/blacklist_unreadable.txt', 0377 );
+			return __DIR__.'/blacklist_unreadable.txt';
+		});
+	} 
+
+	public function test_blacklisted() {
 		// Replace this with some actual testing code.
 		$result = wp_remote_get('https://connect.advancedcustomfields.com');
 
 		$this->assertTrue( is_wp_error( $result ) );
+	}
+
+	public function test_not_blacklisted() {
+		// Replace this with some actual testing code.
+		$result = wp_remote_get('https://google.fr');
+
+		$this->assertFalse( is_wp_error( $result ) );
 	}
 
 	public function test_filter() {
@@ -33,15 +61,7 @@ class WP_HTTP_BLACKLIST extends WP_UnitTestCase {
 	}
 
 	public function test_file_readable() {
-		add_filter('wp_http_blacklist_file', function() {
-			@unlink( __DIR__.'/blacklist_readable.txt' );
-			file_put_contents( __DIR__.'/blacklist_readable.txt', 
-				'google.fr  
-				beapi.fr
-				' 
-			);
-			return __DIR__.'/blacklist_readable.txt';
-		});
+		$this->filter_file_redable();
 
 		// New entry with space ok
 		$result = wp_remote_get('https://google.fr');
@@ -53,28 +73,27 @@ class WP_HTTP_BLACKLIST extends WP_UnitTestCase {
 		$this->assertTrue( is_wp_error( $result ) );
 		$this->assertEquals( $result->get_error_code(), 'http_request_blocked' );
 
+		@unlink( __DIR__.'/blacklist_readable.txt' );
+	}
+
+	public function test_file_readable_removed_default_blocked() {
+		$this->filter_file_redable();
 
 		// Old entry ok
 		$result = wp_remote_get('https://connect.advancedcustomfields.com');
 		$this->assertFalse( is_wp_error( $result ) );
+
+		@unlink( __DIR__.'/blacklist_readable.txt' );
+
 	}
 
 	public function test_file_unreadable() {
-		add_filter('wp_http_blacklist_file', function() {
-			@unlink( __DIR__.'/blacklist_unreadable.txt' );
-			file_put_contents( __DIR__.'/blacklist_unreadable.txt', 
-				'google.fr  
-				beapi.fr
-				' 
-			);
-			chmod(  __DIR__.'/blacklist_unreadable.txt', 0377 );
-			return __DIR__.'/blacklist_unreadable.txt';
-		});
+		$this->filter_file_unredable();
 
 		// New entry with space ok
 		$result = wp_remote_get('https://google.fr');
 		$this->assertFalse( is_wp_error( $result ) );
-
+		@unlink( __DIR__.'/blacklist_unreadable.txt' );
 	}
 
 }
