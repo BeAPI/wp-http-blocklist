4385322b7f331607346421e88af86213ad1ee78f
Nicolas Juen <njuen@beapi.fr>
2020-05-28 17:34:10 +0200

    Use constants for the file path

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -8,53 +8,66 @@
 /**
  * Basic plugin tests.
  */
-class WP_HTTP_BLACKLIST extends WP_UnitTestCase {
+class WP_HTTP_BLACKLIST_TEST extends WP_UnitTestCase {
+
+	const READABLE_FILE = __DIR__ . '/blacklist_readable.txt';
+	const UNREADABLE_FILE = __DIR__ . '/blacklist_unreadable.txt';
+
+	/**
+	 * Create a readable file with domains to block
+	 */
 	private function filter_file_redable() {
-		add_filter('wp_http_blacklist_file', function() {
-			@unlink( __DIR__.'/blacklist_readable.txt' );
-			file_put_contents( __DIR__.'/blacklist_readable.txt', 
-				'google.fr  
+		@unlink( self::READABLE_FILE );
+		file_put_contents( self::READABLE_FILE,
+			'google.fr  
 				beapi.fr
-				' 
-			);
-			return __DIR__.'/blacklist_readable.txt';
-		});
+				'
+		);
+
+		add_filter( 'wp_http_blacklist_file', function () {
+			return self::READABLE_FILE;
+		} );
 	}
 
+	/**
+	 *
+	 */
 	private function filter_file_unredable() {
-		add_filter('wp_http_blacklist_file', function() {
-			@unlink( __DIR__.'/blacklist_unreadable.txt' );
-			file_put_contents( __DIR__.'/blacklist_unreadable.txt', 
-				'google.fr  
+		@unlink( self::UNREADABLE_FILE );
+		file_put_contents( self::UNREADABLE_FILE,
+			'google.fr  
 				beapi.fr
-				' 
-			);
-			chmod(  __DIR__.'/blacklist_unreadable.txt', 0377 );
-			return __DIR__.'/blacklist_unreadable.txt';
-		});
-	} 
+				'
+		);
+		chmod( self::UNREADABLE_FILE, 0377 );
+
+		add_filter( 'wp_http_blacklist_file', function () {
+			return self::UNREADABLE_FILE;
+		} );
+	}
 
 	public function test_blacklisted() {
 		// Replace this with some actual testing code.
-		$result = BEAPI\WPHTTPBlacklist\pre_http_request(false, [], 'https://connect.advancedcustomfields.com');
+		$result = BEAPI\WPHTTPBlacklist\pre_http_request( false, [], 'https://connect.advancedcustomfields.com' );
 
 		$this->assertTrue( is_wp_error( $result ) );
 	}
 
 	public function test_not_blacklisted() {
 		// Replace this with some actual testing code.
-		$result = BEAPI\WPHTTPBlacklist\pre_http_request(false, [], 'https://google.fr');
+		$result = BEAPI\WPHTTPBlacklist\pre_http_request( false, [], 'https://google.fr' );
 
 		$this->assertFalse( $result );
 	}
 
 	public function test_filter() {
-		add_filter('wp_http_blacklist', function( $blacklist ) {
+		add_filter( 'wp_http_blacklist', function ( $blacklist ) {
 			$blacklist[] = 'google.fr';
-			return $blacklist;
-		});
 
-		$result = BEAPI\WPHTTPBlacklist\pre_http_request(false, [], 'https://google.fr');
+			return $blacklist;
+		} );
+
+		$result = BEAPI\WPHTTPBlacklist\pre_http_request( false, [], 'https://google.fr' );
 
 		$this->assertTrue( is_wp_error( $result ) );
 
@@ -64,35 +77,35 @@ class WP_HTTP_BLACKLIST extends WP_UnitTestCase {
 		$this->filter_file_redable();
 
 		// New entry with space ok
-		$result = BEAPI\WPHTTPBlacklist\pre_http_request(false, [], 'https://google.fr');
+		$result = BEAPI\WPHTTPBlacklist\pre_http_request( false, [], 'https://google.fr' );
 		$this->assertTrue( is_wp_error( $result ) );
 		$this->assertEquals( $result->get_error_code(), 'http_request_blocked' );
 
 		// New entry with space ok
-		$result = BEAPI\WPHTTPBlacklist\pre_http_request(false, [], 'https://beapi.fr');
+		$result = BEAPI\WPHTTPBlacklist\pre_http_request( false, [], 'https://beapi.fr' );
 		$this->assertTrue( is_wp_error( $result ) );
 		$this->assertEquals( $result->get_error_code(), 'http_request_blocked' );
 
-		@unlink( __DIR__.'/blacklist_readable.txt' );
+		@unlink( __DIR__ . '/blacklist_readable.txt' );
 	}
 
 	public function test_file_readable_removed_default_blocked() {
 		$this->filter_file_redable();
 
 		// Old entry ok
-		$result = BEAPI\WPHTTPBlacklist\pre_http_request(false, [], 'https://connect.advancedcustomfields.com');
+		$result = BEAPI\WPHTTPBlacklist\pre_http_request( false, [], 'https://connect.advancedcustomfields.com' );
 		$this->assertFalse( $result );
 
-		@unlink( __DIR__.'/blacklist_readable.txt' );
+		@unlink( __DIR__ . '/blacklist_readable.txt' );
 	}
 
 	public function test_file_unreadable() {
 		$this->filter_file_unredable();
 
 		// New entry with space ok
-		$result = BEAPI\WPHTTPBlacklist\pre_http_request(false, [], 'https://google.fr');
+		$result = BEAPI\WPHTTPBlacklist\pre_http_request( false, [], 'https://google.fr' );
 		$this->assertFalse( $result );
-		@unlink( __DIR__.'/blacklist_unreadable.txt' );
+		@unlink( __DIR__ . '/blacklist_unreadable.txt' );
 	}
 
 }
